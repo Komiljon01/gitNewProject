cfad22354bb12896969d43fd39a2b7a728d0e0c8
Komiljon <nortoshovk@gmail.com>
2021-10-11 20:09:49 +0500

    git project

---
+++ script.js
@@ -0,0 +1,15 @@
+
+for (let i = 1; i <= 15; i++) {
+    if(i % 2 == 0) {
+        console.log(i + " ta idish yuvildi");
+    }
+}
+
+function foo(x, y) {
+    return x * y;
+}
+console.log(foo(5,6));
+
+
+
+
